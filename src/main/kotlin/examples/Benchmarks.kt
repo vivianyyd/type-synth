63268b24ce99d6bb97585e69b84e737a003d7046
Vivian Ding <vyd2@cornell.edu>
2024-09-12 23:29:38 -0400

    checkpoint

--- src/main/kotlin/examples/Benchmarks.kt
+++ src/main/kotlin/examples/Benchmarks.kt
@@ -23,32 +23,32 @@ data class TestQuery(val  functions:List<Func>, val names:List<String> , val que
       }
     } 
     fun runTest(){
-        for(key in map.keys){
-            println(System.currentTimeMillis())
-        var file: String = path+"/"+key+".txt"
-        var func = map.get(key)!!
-        var ifac = InputFactory(func, query)
-        val synth = callSketch(ifac.synthInput(listOf(), mapOf()))
-        var res = OutputParser(synth, ifac).parseProperty()
-        if (res !is Result.Ok) return;
-        var phi = res.value
-        //////this is where we need to put in the redirect of output and send it to path
-        File(file).writeText("\nInitial synthesized property: $phi")
-        while(true){
-            val precision = callSketch(ifac.precisionInput(phi, listOf(), listOf(), mapOf()))
-            val op = OutputParser(precision, ifac)
-            val result = op.parseProperty()
-            for(input in func.posExamples){
-                File(file).appendText("\nExample:     "+input)
-            }
-            if (result is Result.Ok) {
-                phi = result.value
-                File(file).appendText("\nProperty with increased precision: $phi")
-                ifac = ifac.withNegEx(op.parseNegExPrecision())
-            }
-            else break
-        }
-        println(System.currentTimeMillis())
-        }
+//        for(key in map.keys){
+//            println(System.currentTimeMillis())
+//        var file: String = path+"/"+key+".txt"
+//        var func = map.get(key)!!
+//        var ifac = InputFactory(func, query)
+//        val synth = callSketch(ifac.synthInput(listOf(), mapOf()))
+//        var res = OutputParser(synth, ifac).parseProperty()
+//        if (res !is Result.Ok) return;
+//        var phi = res.value
+//        //////this is where we need to put in the redirect of output and send it to path
+//        File(file).writeText("\nInitial synthesized property: $phi")
+//        while(true){
+//            val precision = callSketch(ifac.precisionInput(phi, listOf(), listOf(), mapOf()))
+//            val op = OutputParser(precision, ifac)
+//            val result = op.parseProperty()
+//            for(input in func.posExamples){
+//                File(file).appendText("\nExample:     "+input)
+//            }
+//            if (result is Result.Ok) {
+//                phi = result.value
+//                File(file).appendText("\nProperty with increased precision: $phi")
+//                ifac = ifac.withNegEx(op.parseNegExPrecision())
+//            }
+//            else break
+//        }
+//        println(System.currentTimeMillis())
+//        }
     } 
 }
