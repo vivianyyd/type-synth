44b82b84da99cf79a233e6dab53e8aedca67648e
Vivian Ding <vyd2@cornell.edu>
2024-09-30 13:30:59 -0400

    Fix unify bugs

--- src/main/kotlin/enumgen/Unify.kt
+++ src/main/kotlin/enumgen/Unify.kt
@@ -5,6 +5,7 @@ typealias Context = MutableMap<Variable, Type>
 fun err(map: Context) = Pair(Error, map)
 
 class Unify {
+    /** Assumes [target] is not a variable with an entry in [map]. */
     private fun unifyVar(v: Variable, target: Type, map: Context): Pair<Type, Context> =
         map[v]?.let { unify(it, target, map) } ?: run {
             map[v] = target
@@ -15,8 +16,7 @@ class Unify {
         is Variable -> {
             when (b) {
                 is Variable -> { // Unwrap b if possible
-                    // TODO check if this unwrapping is right
-                    map[b]?.let { unify(it, a, map) } ?: unifyVar(a, b, map)
+                    map[b]?.let { unify(a, it, map) } ?: unifyVar(a, b, map)
                 }
                 is Function, is Node -> unifyVar(a, b, map)
                 Error -> err(map)
@@ -27,9 +27,12 @@ class Unify {
                 is Variable -> unifyVar(b, a, map)
                 is Function -> {
                     val (param, innerContext) = unify(a.param, b.param, map)
-                    val output = unify(a.out, b.out, innerContext)
-                    Pair(Function(param, output.first), map)  // TODO check this is the correct context to return
-                }
+                    val (outputType, outerContext) = unify(a.out, b.out, innerContext)
+                    Pair(
+                        Function(param, outputType),
+                        outerContext  // If unifying inputs of (a->a)->a and (int->int)->int, need to store a:=int
+                    )
+                }  // TODO think about variable shadowing. I think a->(a->a) is fine! what about unifying a-> (a->b) if they have diff names. should each type just have disjoint sets of variables?
                 is Node, Error -> err(map)
             }
         }
@@ -37,19 +40,19 @@ class Unify {
             is Variable -> unifyVar(b, a, map)
             is Node -> {
                 if (a.label != b.label || a.typeParams.size != b.typeParams.size) err(map)
-                else {
-                    for (i in 0 until a.typeParams.size) {
-                        // params should be independent, so the var map shouldn't change between them
-                        // TODO think about variable binding here more
-                        val (child, _) = unify(a.typeParams[i], b.typeParams[i], map)
-                        when (child) {
-                            Error -> err(map)
-                            else -> {}
-                        }
-                    }
-                    // TODO we actually have to make a new type containing all the results of unifying the children
-                    Pair(a, map)
+                var currMap = map
+                val (params, maps) = (0 until a.typeParams.size).map {
+                    val (param, newMap) = unify(a.typeParams[it], b.typeParams[it], currMap)
+                    currMap = newMap
+                    Pair(param, newMap)
+                }.unzip()
+                if (Error in params) {
+                    val e = params.indexOf(Error)
+                    // If we failed in the middle, return the map causing the error
+                    err(if (e == 0) map else maps[e - 1])
                 }
+                // If unifying first param of dict(a,b)->a with dict(int,bool)->int, need to keep a:=int to unify output
+                Pair(Node(a.label, params), currMap)
             }
             is Function, Error -> err(map)
         }
