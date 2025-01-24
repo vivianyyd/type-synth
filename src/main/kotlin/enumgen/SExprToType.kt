c6748bc270b91f2d39319cfd4c71cc8af4da4429
Vivian Ding <vyd2@cornell.edu>
2025-01-24 18:41:03 -0500

    Parse types written as sexps

---
+++ src/main/kotlin/enumgen/SExprToType.kt
@@ -0,0 +1,25 @@
+package enumgen
+
+fun SExpr.toType(): Type = when (this) {
+    is SExpr.Atm -> {
+        assert (this.value != "->")
+        Variable(this.value)
+    }
+    is SExpr.Lst -> {
+        assert (this.elements.isNotEmpty())
+        assert(this.elements[0] is SExpr.Atm)
+        val fst = this.elements[0] as SExpr.Atm
+        if (fst.value == "->") {
+            assert(this.elements.size==3)
+            Function(left=this.elements[1].toType(), rite = this.elements[2].toType())
+        }
+        else {
+            LabelNode(label=fst.value, params=this.elements.drop(1).map{it.toType()})
+        }
+    }
+}
+
+    /*
+Lst(elements=[Atm(value=->), Atm(value=a), Lst(elements=[Atm(value=->), Lst(elements=[Atm(value=l), Atm(value=a)]), Lst(elements=[Atm(value=l), Atm(value=a)])])])
+Lst(elements=[Atm(value=lbl)])
+     */
