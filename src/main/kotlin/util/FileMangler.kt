cad2775f0c89eaabf7210fce3f9e3f2ed92286c3
Vivian Ding <vyd2@cornell.edu>
2025-03-29 15:31:14 -0400

    Some renaming

--- src/main/kotlin/util/FileMangler.kt
+++ src/main/kotlin/util/FileMangler.kt
@@ -8,17 +8,16 @@ private fun profilingResultPath(testName: String) = join("results", "$testName.c
 
 // TODO can pass this in main function
 private val customCodeGenerator = join("../applications/sketch-1.7.6/sketch-frontend/customcodegen.jar")
-private val generationPath = join("src", "main", "sketch", "symbolicgen", "generated")
+private val sketchGenerationPath = join("src", "main", "sketch", "symbolicgen", "generated")
 
 // TODO use --fe-inc so include statements don't have absolute paths
-private fun path(dir: String, testName: String) = join(generationPath, dir, "$testName.sk")
-private fun inputPath(testName: String) = path("input", testName)
-private fun outputPath(testName: String) = path("output", testName)
-
+private fun sketchPath(dir: String, testName: String) = join(sketchGenerationPath, dir, "$testName.sk")
+private fun sketchInputPath(testName: String) = sketchPath("input", testName)
+private fun sketchOutputPath(testName: String) = sketchPath("output", testName)
 private fun write(path: String, contents: String) = File(path).printWriter().use { it.println(contents) }
 
 fun callSketch(input: String, testName: String): String {
-    write(inputPath(testName), input)
+    write(sketchInputPath(testName), input)
     val flags = listOf(
         "--fe-custom-codegen $customCodeGenerator",
         "--slv-parallel",
@@ -26,12 +25,12 @@ fun callSketch(input: String, testName: String): String {
         "--slv-nativeints",
         "--bnd-inline-amnt 5"
     ).joinToString(separator = " ")
-    val out = "sketch ${inputPath(testName)} $flags".runCommand()
+    val out = "sketch ${sketchInputPath(testName)} $flags".runCommand()
         ?: throw Exception("I'm sad")
-    write(outputPath(testName), out)
+    write(sketchOutputPath(testName), out)
     return out
 }
 
-fun readOutput(testName: String) = File(outputPath(testName)).readText()
+fun readSketchOutput(testName: String) = File(sketchOutputPath(testName)).readText()
 
 fun writeProfiling(output: String, testName: String) = write(profilingResultPath(testName), output)
