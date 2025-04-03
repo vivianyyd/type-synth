f9aa7e0c8273cba1f3fc75f0fb770c451c388021
Vivian Ding <vyd2@cornell.edu>
2025-04-03 13:56:19 -0400

    Minor code for running

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,26 @@
 import symbolicgen.SymbolicTypeBuilder
 import symbolicgen.concretesketcher.ConcreteSketcher
 import symbolicgen.symbolicenumerator.SymbolicEnumerator
-import test.IdTest
+import test.ConsTest
 import util.writeConcretizeInput
 
 const val ROUNDS = 4
 const val RUN_SKETCH = true
 
 fun main() {
-    val query = IdTest.query
-    val oracle = IdTest.oracle
+    val test = ConsTest
+    val query = test.query
+    val oracle = test.oracle
     val b = SymbolicTypeBuilder(query).make
 
     b.printState()
-    val enum = SymbolicEnumerator(query, b)
-    val sketcher = ConcreteSketcher(query, enum.enumerateAll()[0], enum.varTypeIds, oracle)
-    writeConcretizeInput(sketcher.query(), "test")
+    val enum = SymbolicEnumerator(query, b, oracle)
+    val specializedSymbolicTypes = enum.enumerateAll()
+    println(specializedSymbolicTypes.pr())
+    specializedSymbolicTypes.forEachIndexed { i, t ->
+        val sketcher = ConcreteSketcher(query, t, enum.varTypeIds, oracle)
+        writeConcretizeInput(sketcher.query(), "test$i")
+    }
 
 //    val out = if (RUN_SKETCH) callSketch(sketcher.sketchInput(), "test") else readSketchOutput("test")
 //    val (types, time) = (sketcher.parse(out))
