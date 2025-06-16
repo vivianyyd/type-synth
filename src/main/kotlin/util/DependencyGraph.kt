6ce8da8846bb3b8b65fbf96a88642b4034409ede
Vivian Ding <vyd2@cornell.edu>
2025-06-16 16:32:14 -0400

    Delete extraneous code

--- src/main/kotlin/util/DependencyGraph.kt
+++ src/main/kotlin/util/DependencyGraph.kt
@@ -14,21 +14,7 @@ sealed interface Edge
 data class DependencyEdge(val sub: ParameterNode, val sup: ParameterNode) : Edge {
     override fun toString(): String = "$sub -> $sup"
 }
-// TODO is it weird that this is a data class but [LinkEdge] isn't?
 
-/** undirected, otherwise nonempty intersection */
-class LinkEdge(val a: ParameterNode, val b: ParameterNode) : Edge {
-    override fun equals(other: Any?): Boolean {
-        if (other !is LinkEdge) return false
-        return (a == other.a && b == other.b) || (a == other.b && b == other.a)
-    }
-
-    override fun hashCode(): Int {
-        return a.hashCode() + b.hashCode()  // TODO aaa
-    }
-}
-
-/** TODO maybe these should just be a type of dependency */
 data class SelfLoop(val a: ParameterNode) : Edge
 
 // TODO decide if nodes/edges should be constructed in init block or by DepAnalysis class and only stored here
