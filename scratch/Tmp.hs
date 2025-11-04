ce8dafbc709c62652e22d4f6cf10c5e11fefadf7
Vivian Ding <vyd2@cornell.edu>
2025-11-03 23:32:07 -0500

    haskell

--- scratch/Tmp.hs
+++ scratch/Tmp.hs
@@ -1,9 +1,14 @@
 import GHC
 import DynFlags
-import Outputable (Outputable, showPpr)
+import Outputable (Outputable, showPpr, showSDocUnsafe, ppr, text, (<+>))
 import qualified GHC.Paths as Paths
-
 import Data.Functor
+
+import Control.Monad.IO.Class (liftIO)
+import Var             (varType)
+import TcRnTypes       (tcg_binds)
+import Bag             (bagToList)
+
 
 runGhc' :: Ghc a -> IO a
 runGhc' ga = do
@@ -17,22 +22,48 @@ runGhc' ga = do
 
 typeExample :: Ghc TypecheckedModule
 typeExample = do
-    target <- guessTarget "prettyPrint2dList.hs" Nothing
+    target <- guessTarget "Simple.hs" Nothing
     setTargets [target]
     load LoadAllTargets
     modGraph <- depanal [] False
     let modSummary = head (mgModSummaries modGraph)
+    let modSummary = head [ m | m <- mgModSummaries modGraph
+                              , ms_hspp_file m == "Simple.hs" ]
     parsed <- parseModule modSummary
     typechecked <- typecheckModule parsed
     return typechecked
 
 compileExample :: Ghc CoreModule
 --compileExample = compileToCoreModule "prettyPrint2dList.hs"
-compileExample = compileToCoreSimplified "prettyPrint2dList.hs"
+compileExample = compileToCoreSimplified "Simple.hs"
 
 showPpr' :: (Functor m, Outputable a, HasDynFlags m) => a -> m String
 showPpr' a = (flip showPpr) a <$> getDynFlags
 
+printHsBindType :: LHsBind GhcTc -> IO ()
+printHsBindType lb = case unLoc lb of
+  FunBind {fun_id = fid} ->
+    let var = unLoc fid
+    in putStrLn $ showSDocUnsafe $ ppr var <+> text "::" <+> ppr (varType var)
+  _ -> return ()
+
+
 -- main = runGhc' (typeExample >>= showPpr') >>= putStrLn
 
-main = runGhc' (typeExample >>= showPpr' . tm_typechecked_source) >>= putStrLn
+-- main = runGhc' (typeExample >>= showPpr' . tm_typechecked_source) >>= putStrLn
+
+main :: IO ()
+main = runGhc (Just Paths.libdir) $ do
+    dflags <- getSessionDynFlags
+    _ <- setSessionDynFlags dflags
+    -- todo copy code above instead     
+    
+    typechecked <- typeExample
+
+    liftIO $ putStrLn "\n=== Typechecked HsSyn types ==="
+
+    let binds = tm_typechecked_source typechecked
+    --let (tcg, _) = tm_internals_ typechecked
+      --  binds     = tcg_binds tcg
+
+    mapM_ (liftIO . printHsBindType) (bagToList binds)
--- scratch/prettyPrint2dList.hs
+++ scratch/prettyPrint2dList.hs
@@ -1,3 +1,5 @@
+module PrettyPrint2DList where
+
 data X = Y | Z
          deriving (Eq, Show)
 
@@ -5,5 +7,8 @@ type R = [X]
 type W = [R]
 
 example = map (\x -> take x (cycle [Y, Z])) [0..]
+
+foo :: Int -> Int
+foo x = x + 1
 
 main = undefined
