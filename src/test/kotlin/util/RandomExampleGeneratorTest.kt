6c295d4fa5c325c944695ef9351a3cc338c74670
Vivian Ding <vyd2@cornell.edu>
2026-02-11 23:46:16 -0500

    Start random generator test just cus

---
+++ src/test/kotlin/util/RandomExampleGeneratorTest.kt
@@ -0,0 +1,40 @@
+package util
+
+import org.junit.jupiter.api.Test
+import util.io.parseSExpr
+import kotlin.test.assertEquals
+
+class RandomExampleGeneratorTest {
+    @Test
+    fun `generates examples`() {
+        val gen =
+            RandomExampleGenerator(
+                listOf(
+                    // Boolean operations section of stdlib 5.4 docs
+                    "not",
+                    "(&&)",
+                    "(||)",
+                    // Integer arithmetic
+                    "(~-)",
+                    "(~+)",
+                    "succ",
+                    "pred",
+                    "(+)",
+                    "(-)",
+                    "( * )",
+                    "(/)",
+                    "(mod)",
+                    "abs",
+                    "max_int",
+                    "min_int",
+                    // Character operations
+                    "int_of_char",
+                    "char_of_int"
+                )
+            )
+        // each name gotta have 5 examples
+        val input = "(def (square x) (* x x))"
+        val result = parseSExpr(input)
+        assertEquals(result.toString(), input)
+    }
+}
