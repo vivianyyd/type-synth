b0d485a946ef9e5068a710347093f5f7ea8ce1be
Vivian Ding <vyd2@cornell.edu>
2026-02-11 23:44:46 -0500

    Dictchain sanity check

---
+++ src/test/kotlin/oneast/DictChainTest.kt
@@ -0,0 +1,46 @@
+package oneast
+
+import kotlin.test.Test
+import oneast.searchstrategies.DFSEnumerator
+import util.Logger
+import util.io.parseTest
+
+class DictChainTest {
+    @Test
+    fun `can build query from input file`() {
+        val query = parseTest("dictchain")
+
+        val configuration =
+            Configuration(
+                name = "dictchain",
+                runCVC = true,
+                sizeBound = 20,
+                depthBound = 4,
+                namesPerRound = 10,
+                numSols = Solutions.NumSolutions(1)
+            )
+
+        val logger =
+            Logger(
+                configuration = configuration,
+                logFilename = "tmp.log",
+                logToFile = true,
+                verbosity = 5
+            )
+
+        val engine =
+            Engine(
+                query.examples,
+                { s, q ->
+                    logger.log("Searching $s")
+                    Search(s, q, query.oracle, configuration, ::DFSEnumerator, logger)
+                },
+                { e -> TODO() },
+                logger,
+                namesPerRound = configuration.namesPerRound
+            )
+
+        engine.search().take(1).forEach { logger.log("FIRST SOLUTION: ${it.asMap()}") }
+        logger.finish()
+    }
+}
