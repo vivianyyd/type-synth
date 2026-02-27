a9c17815b0a9ab94c1f4ed99dc071bb5f86333c7
Vivian Ding <vyd2@cornell.edu>
2026-02-27 16:47:02 -0500

    Clarifying comments and some simplification in DFS. Cost of committing is always 1

--- src/main/kotlin/oneast/searchstrategies/DFSEnumerator.kt
+++ src/main/kotlin/oneast/searchstrategies/DFSEnumerator.kt
@@ -1,6 +1,5 @@
 package oneast.searchstrategies
 
-import oneast.Blank
 import oneast.OneUnification
 import oneast.SearchState
 import oneast.SearchStrategy
@@ -15,6 +14,9 @@ class DFSEnumerator(examples: Examples) : SearchStrategy(examples) {
     override fun candidates(
         c: SearchState,
         unification: OneUnification,
+        // TODO There is an implicit invariant/bug where fastforward is never called on a blank at
+        // the top level when we are still making outlines, only later when we are looking for
+        // concrete types. That should be cleaned up and made apparent by the code
         introduceBlanks: Boolean,
         fastForwardBlanks: Boolean,
         sizeBound: Int,
@@ -23,12 +25,6 @@ class DFSEnumerator(examples: Examples) : SearchStrategy(examples) {
         logger: Logger
     ): Sequence<SearchState> {
         if (c.noHoles()) return sequenceOf(c)
-        // TODO consider if I want to fast forward here, or do it later outside this fn
-        //   call. Fast forwarding won't do anything in the first round when we purposefully
-        //   have blanks since no labels yet, and in fact, it is probably actually bad!
-        //   on the other hand, in the future iterations, we may as well fast forward outside
-        //   this function. And in that case, we can combine this line with the c.noHoles()
-        //   check.
 
         fun fastForward(): Sequence<SearchState> {
             val ff = fastForward(c)
@@ -53,26 +49,26 @@ class DFSEnumerator(examples: Examples) : SearchStrategy(examples) {
                 mustBeLeaf = sizeBound <= 1 || depth >= depthBound
             )
             .asSequence()
-            .map { it to c.mapTypeAtIndex(iToFill) { typ -> typ.replace(hole, it) } }
-            .filterNot { (_, newC) -> newC.types[iToFill].invalid() }
-            .flatMap { (replacement, newCandidate) ->
+            .map { c.mapTypeAtIndex(iToFill) { typ -> typ.replace(hole, it) } }
+            .filterNot {
+                // Importantly, this pruning is sound even when we perform it on outlines (before
+                // label arities are computed and holes inserted accordingly). That's because when
+                // we are generating outlines, labels are considered blanks
+                it.types[iToFill].invalid()
+            }
+            .flatMap { newCandidate ->
                 logger.count("Total candidates")
                 val u = posUnification(newCandidate)
                 if (u.ok()) {
-                    // Committing a blank at the top-level is free
-                    val cost =
-                        if (replacement is Blank && newCandidate.types.any { it == replacement }) 0
-                        else 1
                     candidates(
                         newCandidate,
                         u,
                         introduceBlanks,
                         fastForwardBlanks,
-                        sizeBound - cost,
+                        sizeBound - 1,
                         depthBound,
                         loggingSeed,
-                        logger
-                    )
+                        logger)
                 } else emptySequence()
             }
     }
