85aa45738f7e17d9ca0c3f7a8e7b89e069ff1665
Vivian Ding <vyd2@cornell.edu>
2026-03-26 14:53:23 -0400

    Reduce diff

--- src/main/kotlin/oneast/LabelConstraints.kt
+++ src/main/kotlin/oneast/LabelConstraints.kt
@@ -106,8 +106,7 @@ class LabelConstraints(
         // TODO CVC query needs to ask whether it's SAT to carry over leq prev label arities and
         //   still satisfy dep constraints on new ones.
         //   If UNSAT, we need to resolve for arities of all labels that appear in types that
-        // contain
-        //   holes. Update labelArities and SearchState types.
+        //   contain holes. Update labelArities and SearchState types.
         s.labelArities.entries.forEach {}
     }
 
